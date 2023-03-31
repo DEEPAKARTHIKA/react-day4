a0f5323cdb926fc33622cc064343449ea738b033
DEEPAKARTHIKA <108518917+DEEPAKARTHIKA@users.noreply.github.com>
2023-03-31 14:56:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+import GreetingForm from './Component/Greetings'
+
+
+
+export default function App() {
+  return (
+    <div>
+    <GreetingForm/>
+    </div>
+  )
+}
